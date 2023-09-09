80f5b67ea2df2ffafc148bd51333e0f10df555eb
Elizabeth-Jane <enyinnaelizabethjane@gmail.com>
2023-09-09 11:12:26 +0100

    updated clock

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
     document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = daysOfWeek[currentDate.getUTCDay()];
 
 
-function updateClock() {
-        const currentTime = new Date();
-        const hours = currentTime.getHours().toString().padStart(2, '0');
-        const minutes = currentTime.getMinutes().toString().padStart(2, '0');
-        const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+ function updateClock() {
+       const currentTime = new Date();
+    document.querySelector('[data-testid="currentUTCTime"]').textContent = new Date().getTime();
+   }
     
-        document.getElementById('currentTime').textContent = `${hours}:${minutes}:${seconds}`;
-    }
-    
-    updateClock();
+   updateClock();
     
 
     setInterval(updateClock, 1000);
